bc902535924dff1b6ff00a6974cceaf41d62023c
Tugberk Ugurlu <tugberk@outlook.com>
2015-07-07 08:24:39 -0700

    added hello-world and file-read-write samples

---
+++ hello_files.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+    char data[100];
+
+    // open a file in the write mode
+    ofstream outfile;
+    outfile.open("afile.dat");
+
+    cout << "Writing to the file" << endl;
+    cout << "Enter your name: ";
+    cin.getline(data, 100);
+
+    // write inputted data into the file.
+    outfile << data << endl;
+
+    cout << "Enter your age: ";
+    cin >> data;
+    cin.ignore();
+
+
+    // again write inputted data into the file.
+    outfile << data << endl;
+
+    // close the opened file.
+    outfile.close();
+
+    // open a file in read mode
+    ifstream infile;
+    infile.open("afile.dat");
+
+    cout << "Reading from the file" << endl;
+    infile >> data;
+
+    // write data at the screen
+    cout << data << endl;
+
+    // close the opened file
+    infile.close();
+
+    return 0;
+}
